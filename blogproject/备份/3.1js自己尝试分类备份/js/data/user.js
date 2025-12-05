268b5f69e067b8013d8ae215f1685102bcf36d5a
wantoknow717 <wantoknow717@outlook.com>
2025-12-05 21:45:13 +0800

    Delete blogproject/备份 directory

--- blogproject/备份/3.1js自己尝试分类备份/js/data/user.js
+++
@@ -1,5 +0,0 @@
-export const userLoginList = [
-    {username:"张三", password:"123456"},
-    {username:"李四", password:"234567"},
-    {username:"王五", password:"xcvbnm"},    
-]   // 模拟用户列表
--- blogproject/备份/3.1js自己尝试分类备份/js/util/show.js
+++
@@ -1,8 +0,0 @@
-export function show(element) {
-    setTimeout(()=>element.classList.add('show'),100)
-    // 自动消失
-    setTimeout(()=>{
-        element.classList.remove('show')
-        setTimeout(() => element.remove(), 300)
-    },2000)
-}
